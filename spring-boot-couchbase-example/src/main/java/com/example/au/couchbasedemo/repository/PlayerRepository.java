751de6ede4cd89c7402ded06ee2f357d84604293
Ankit sharma <77070652+ankit25sharma@users.noreply.github.com>
2021-01-19 16:33:21 +0530

    Delete spring-boot-couchbase-example directory

--- spring-boot-couchbase-example/src/main/java/com/example/au/couchbasedemo/repository/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.au.couchbasedemo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.au.couchbasedemo.model.Player;
-
-public interface PlayerRepository extends CrudRepository<Player,String>{
-    
-}
